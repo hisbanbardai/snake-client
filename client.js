04d0734928b09f515c9f03bd95599ee875102698
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-20 18:32:54 -0600

    Experimented how to send moves to the server in order to move the snake

--- client.js
+++ client.js
@@ -7,6 +7,28 @@ const connect = function () {
     port: 50541,
   });
 
+  // const moveUp = () => {
+  //   return new Promise((resolve, reject) => {
+  //     conn.on("connect", () => {
+  //       setTimeout(() => {
+  //         conn.write("Move: up");
+  //         return resolve("Snake moved up");
+  //       }, 3000);
+  //     });
+  //   });
+  // };
+
+  // const moveRight = () => {
+  //   return new Promise((resolve, reject) => {
+  //     conn.on("connect", () => {
+  //       setTimeout(() => {
+  //         conn.write("Move: right");
+  //         return resolve("Snake moved right");
+  //       }, 5000);
+  //     });
+  //   });
+  // };
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
@@ -18,7 +40,16 @@ const connect = function () {
     console.log("Successfully connected to game server")
   );
 
+  //Sending our initials for the game to the server
   conn.on("connect", () => conn.write("Name: HSB"));
+
+
+  //Sending our moves to the server to move the snake
+
+  // moveUp()
+  //   .then((data) => console.log(data))
+  //   .then(moveRight)
+  //   .then((data) => console.log(data));
 
   return conn;
 };
