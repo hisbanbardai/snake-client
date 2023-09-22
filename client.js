db3afda8aca70098568ba49c39b740d1c653aa2d
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-22 09:47:22 -0600

    Displayed game instructions after client is connected

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const net = require("net");
-const { IP, PORT } = require("./constants");
+const { IP, PORT, MOVEMENT_KEYS, SPECIAL_KEYS } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function (name) {
@@ -16,9 +16,12 @@ const connect = function (name) {
     console.log(`Server says: ${data}`);
   });
 
-  //printing a message when the connection is successfully established
+  //printing a message when the connection is successfully established along with the instructions to play
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
+    console.log("Following are the keys to move the snake: \n",MOVEMENT_KEYS);
+    console.log("\nFollowing are the special keys to send canned messages to the server for everyone to see: \n",SPECIAL_KEYS);
+
     //Sending our initials for the game to the server
     conn.write(`Name: ${name}`);
   });
